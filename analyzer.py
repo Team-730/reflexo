a77c4ca438efb830bf371c720aa0e641644951c3
Destroyer Rules <kuskov779@gmail.com>
2021-04-08 12:07:30 +0300

    Edit analyzer.py for working with bot, added a function ready_msg for it

--- analyzer.py
+++ analyzer.py
@@ -2,30 +2,12 @@ from dostoevsky.tokenization import RegexTokenizer
 from dostoevsky.models import FastTextSocialNetworkModel
 
 tokenizer = RegexTokenizer()
-tokens = tokenizer.split('всё очень плохо')  # [('всё', None), ('очень', None), ('плохо', None)]
-
 model = FastTextSocialNetworkModel(tokenizer=tokenizer)
-
-ids = [
-    '834924982',
-    '472348234',
-    '472348234'
-]
-messages = [
-    'день прошел отлично',
-    'ну так себе',
-    'норм'
-]
-mood = [
-    'happy+=0,1',
-    'neutral+=0,2; negative+=0,1',
-    'neutral+=0,2; positive+=0,05'
-]
-
-results = model.predict(messages, k=2)
-
-for message, sentiment in zip(messages, results):
-    # привет -> {'speech': 1.0000100135803223, 'skip': 0.0020607432816177607}
-    # люблю тебя!! -> {'positive': 0.9886782765388489, 'skip': 0.005394937004894018}
-    # малолетние дебилы -> {'negative': 0.9525841474533081, 'neutral': 0.13661839067935944}]
-    print(message, '->', sentiment)
+tokens = tokenizer.split('всё очень плохо') 
+def ready_msg(messages):
+    results = model.predict(messages, k=2)
+    sentiments = []
+    for message, sentiment in zip(messages, results):
+        sentiments.append(sentiment)
+    return(sentiments)
+ready_msg(['все круто'])
