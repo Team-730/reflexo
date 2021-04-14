37c08236011382d7e8c2159c0e02acca200a54d4
Uknown-creator <kuskov779@gmail.com>
2021-04-14 18:14:55 +0300

    Add web

--- test.py
+++ test.py
@@ -1,6 +1,3 @@
-from PIL import Image
-from transforms import RGBTransform # from source code mentioned above
-
-lena = Image.open("sticker1.jpg")
-lena = lena.convert('RGB') # ensure image has 3 channels
-lena
+import database as DB
+db = DB()
+print(db.getMatrix('1'))
