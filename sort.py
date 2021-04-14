0a489b4c7f840681203619974ed0f2bf7df4cfac
Uknown-creator <kuskov779@gmail.com>
2021-04-14 15:34:12 +0300

    Update files

--- sort.py
+++ sort.py
@@ -1,34 +1,31 @@
-from PIL import Image
-from transforms import RGBTransform 
-
 stickers = {
-    '111': 'yellow.jpg'
-    '112': 'yellow.jpg'
-    '121': 'yellow.jpg'
-    '122': 'pink.jpg'
-    '131': 'pink.jpg'
-    '132': 'pink.jpg'
-    '113': 'purple.jpg'
-    '123': 'purple.jpg'
-    '133' 
-    '211'
-    '221'
-    '222'
-    '232'
-    '212'
-    '213'
-    '223'
-    '233'
-    '231'
-    '311'
-    '321'
-    '312'
-    '322'
-    '332'
-    '331'
-    '333'
-    '323'
-    '313'
+    '111': 'yellow.jpg',
+    '112': 'yellow.jpg',
+    '121': 'yellow.jpg',
+    '122': 'pink.jpg',
+    '131': 'pink.jpg',
+    '132': 'pink.jpg',
+    '113': 'purple.jpg',
+    '123': 'purple.jpg',
+    # '133'
+    # '211'
+    # '221'
+    # '222'
+    # '232'
+    # '212'
+    # '213'
+    # '223'
+    # '233'
+    # '231'
+    # '311'
+    # '321'
+    # '312'
+    # '322'
+    # '332'
+    # '331'
+    # '333'
+    # '323'
+    # '313'
 }
 
 def get_values():
